4043afe238ff0ca0ac66ac43419c39f03f95d9aa
Amin Hassan <125133476+Amin-192@users.noreply.github.com>
2024-10-06 21:00:34 +0300

    added a working database

--- studymate/base/views.py
+++ studymate/base/views.py
@@ -2,20 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models  import Room
 
-rooms = [
-    {
-        'name': 'Room buss',
-        'description': 'This is room 1 description',
-    },
-    {
-        'name': 'Room 2',
-        'description': 'This is room 2 description',
-    },
-    # Add more rooms here...
-]
+
+
 def home(request): 
-    return render(request, 'base/home.html', {'rooms': rooms})
+    Rooms = Room.objects.all()  # Fetch all Room objects from the database
+    return render(request, 'base/home.html', {'Rooms': Rooms})
+
+
     
 def room(request):
     return render(request, 'base/room.html')
