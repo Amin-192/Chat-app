6343d2180e5246b514de508d9c03908ac3fa8b62
Amin Hassan <125133476+Amin-192@users.noreply.github.com>
2024-10-06 19:52:54 +0300

    easier set up to and more front end fucntionalitites

--- studymate/base/views.py
+++ studymate/base/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+
+rooms = [
+    {
+        'name': 'Room buss',
+        'description': 'This is room 1 description',
+    },
+    {
+        'name': 'Room 2',
+        'description': 'This is room 2 description',
+    },
+    # Add more rooms here...
+]
 def home(request): 
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', {'rooms': rooms})
     
 def room(request):
     return render(request, 'base/room.html')
